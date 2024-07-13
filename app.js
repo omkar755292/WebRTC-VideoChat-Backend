fcb3be64ec7e42f2bbd5dd9820b1c562c6e0b0c5
omkar <omkar13021@gmail.com>
2024-07-13 17:30:20 +0530

    update

--- app.js
+++ app.js
@@ -4,14 +4,22 @@ const cors = require('cors');
 const errorHandler = require('./middleware/errorHandler');
 const DBConnect = require('./config/connectionDB');
 const authRouter = require('./routes/authRouter');
+const {Server} = require('socket.io')
+
 
 
 env.config() //Configuring Hostname and Port
 const hostname = process.env.HOSTNAME || 'localhost';
 const port = process.env.PORT || 5000;
+const socket_port = process.env.SOCKET_PORT || 5003;
 
 DBConnect();
 const app = express();
+const io = new Server(socket_port,
+    {
+        cors: true
+    }
+);
 
 app.use((req, res, next) => {
     console.log(`Requested API: ${req.method} ${req.originalUrl}`);
@@ -28,3 +36,10 @@ app.use(errorHandler);
 app.listen(port, (req, res) => {
     console.log(`server listen on port http://${hostname}:${port}`);
 });
+
+io.on("connection",(socket)=>{
+    console.log(`Socket Connected `, socket.id);
+    socket.on('room:join', data=>{
+        console.log(data);
+    })
+})
